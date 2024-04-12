7a5ef3b037d569014d3ac4b8241669e875206523
Zack Dorward <zdorward1@gmail.com>
2024-04-12 00:20:10 -0600

    add stuff

--- src/components/rules.rs
+++ src/components/rules.rs
@@ -19,7 +19,13 @@ pub fn connect4_rules(props: &Connect4RuleProps) -> Html {
     html!{
         <div class="max-w-4xl mx-auto p-5">
             <h1 class="text-3xl font-bold text-center text-gray-800 mb-4">{"Connect 4 Rules"}</h1>
-            <button {onclick} class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">{"Play Connect 4"}</button>
+            <p>{"Connect 4 is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs."}</p>
+
+            <button 
+                {onclick} 
+                class="mt-4 py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+            {"Play Connect 4"}</button>
         </div>
     }
 }
@@ -34,7 +40,12 @@ pub fn toot_and_otto_rules(props: &TootOttoRuleProps) -> Html {
     html! {
         <div class="max-w-4xl mx-auto p-5">
             <h1 class="text-3xl font-bold text-center text-gray-800 mb-4">{"Toot and Otto Rules"}</h1>
-            <button {onclick} class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">{"Play Toot and Otto"}</button>
+            <p>{"Toot and Otto is a two-player strategy game where players take turns drawing lines between dots on a grid. The goal is to form squares. When a player completes a square, they write their initial in it and take another turn. The game ends when all the squares have been completed, and the player with the most squares wins."}</p>
+            <button 
+                {onclick} 
+                class="mt-4 py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >{"Play Toot and Otto"}
+            </button>
         </div>
     }
 }
