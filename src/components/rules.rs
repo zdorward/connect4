adf092fe43b79d32d4740f8c065c7a60b4560331
Zack Dorward <zdorward1@gmail.com>
2024-04-12 00:27:59 -0600

    add rules

--- src/components/rules.rs
+++ src/components/rules.rs
@@ -17,15 +17,16 @@ pub fn connect4_rules(props: &Connect4RuleProps) -> Html {
     };
 
     html!{
-        <div class="max-w-4xl mx-auto p-5">
+        <div class="max-w-4xl mx-auto p-5 flex flex-col items-center">
             <h1 class="text-3xl font-bold text-center text-gray-800 mb-4">{"Connect 4 Rules"}</h1>
-            <p>{"Connect 4 is a two-player connection game in which the players first choose a color and then take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs."}</p>
+            <p>{"Connect 4 is a two-player game where players take turns dropping colored discs from the top into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective is to be the first to form a horizontal, vertical, or diagonal line of four of one's own discs."}</p>
 
             <button 
                 {onclick} 
                 class="mt-4 py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-            {"Play Connect 4"}</button>
+            >
+                {"Play Connect 4"}
+            </button>
         </div>
     }
 }
@@ -38,13 +39,14 @@ pub fn toot_and_otto_rules(props: &TootOttoRuleProps) -> Html {
     };
 
     html! {
-        <div class="max-w-4xl mx-auto p-5">
+        <div class="max-w-4xl mx-auto p-5 flex flex-col items-center">
             <h1 class="text-3xl font-bold text-center text-gray-800 mb-4">{"Toot and Otto Rules"}</h1>
-            <p>{"Toot and Otto is a two-player strategy game where players take turns drawing lines between dots on a grid. The goal is to form squares. When a player completes a square, they write their initial in it and take another turn. The game ends when all the squares have been completed, and the player with the most squares wins."}</p>
+            <p>{"Toot and Otto is a two-player word-building game. Players take turns placing either a 'T' or an 'O' on a grid, aiming to spell out 'TOOT' (Player 1) or 'OTTO' (Player 2). Each letter can only be used six times, and players aim to create their words vertically, horizontally, or diagonally."}</p>
             <button 
                 {onclick} 
                 class="mt-4 py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >{"Play Toot and Otto"}
+            >
+                {"Play Toot and Otto"}
             </button>
         </div>
     }
